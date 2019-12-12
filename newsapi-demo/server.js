4725cf226a91c27e4a9657bd09a2df383010bf9f
CodeSample <gh-webhk@ahk.jp>
2019-12-12 15:30:11 +0900

    Create server.js

---
+++ newsapi-demo/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const NewsAPI = require('newsapi');
+
+//自分のAPIキーに置き換えてください
+const newsapi = new NewsAPI('***********************');
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+//News APIを利用してトップニュースを取得する
+app.get("/data", (req, res) => {
+  newsapi.v2.topHeadlines({
+    country: 'jp',
+    category: 'technology',
+    pageSize: 40
+  }).then(news => res.send(news));
+});
+
+app.listen(3000, () => console.log('listening on port ' + 3000));
