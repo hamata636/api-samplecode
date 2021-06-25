11b4b3957b2492daa533c61433f01f4451aad029
hamata636 <79074223+hamata636@users.noreply.github.com>
2021-06-25 11:06:11 +0900

    Update server.js

--- newsapi-demo/server.js
+++ newsapi-demo/server.js
@@ -3,7 +3,7 @@ const app = express();
 const NewsAPI = require('newsapi');
 
 //自分のAPIキーに置き換えてください
-const newsapi = new NewsAPI('***********************');
+const newsapi = new NewsAPI('e81d3e77de7b44d79b2117826855227d');
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
